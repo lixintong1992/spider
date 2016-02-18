9b1d9a839945ead96eaa0cb8469de4e4ee25680d
lixintong1992 <18392181430@163.com>
2016-02-18 20:48:52 +0800

    代码搬运，下载贴吧图片的爬虫
    代码搬运，下载贴吧图片的爬虫

---
+++ first.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+'Download tieba picture html'
+
+import urllib
+import re
+
+_author__ = 'Lust'
+
+
+def getHtml(url):
+    page = urllib.urlopen(url)
+    html = page.read()
+    return html
+
+
+def getImg(html):
+    reg = r'src="(.+?\.jpg)" pic_ext'
+    imgre = re.compile(reg)
+    imglist = re.findall(imgre, html)
+    x = 0
+    for imgurl in imglist:
+        urllib.urlretrieve(imgurl, r'C:\Users\Administrator\Desktop\spider\data\%s.jpg' % x)
+        x += 1
+
+html = getHtml("http://tieba.baidu.com/p/2460150866")
+
+print getImg(html)
