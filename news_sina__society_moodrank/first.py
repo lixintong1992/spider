f0419dde08a26969387ef0d1ce65d394ae1fc43a
lixintong.1101 <lixintong_job@163.com>
2018-11-06 01:09:44 +0800

    get news mood top lid

---
+++ news_sina__society_moodrank/first.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+
+import re
+from urllib import request
+
+_author__ = 'lixintong'
+
+# url = "http://news.sina.com.cn/society/moodrank/20161110.shtml"
+url = "http://top.news.sina.com.cn/ws/GetTopDataList.php?top_type=day&top_cat=mood_moodnews1&top_time=20161110&top_show_num=8&top_order=DESC&short_title=1"
+
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64)'
+headers = {'User-Agent': user_agent}
+
+# url_request = request.Request(url, headers=headers)
+response = request.urlopen(url)
+
+a = response.read()
